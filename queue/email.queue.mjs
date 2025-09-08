c690b1e039c4e8d4f03549861df5a3dccf375006
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-09-08 14:57:10 +0530

    queue email sender added

---
+++ queue/email.queue.mjs
@@ -0,0 +1,12 @@
+import { Queue } from 'bullmq';
+import redis from '../redis.mjs';
+
+const emailQueue = new Queue('email_queue', {
+  connection: redis,
+  // defaultJobOptions: {
+  //   attempts: 3,
+  //   backoff: {
+  //     type: 'exponential', delay: 5000
+  //   },
+  // }
+});
