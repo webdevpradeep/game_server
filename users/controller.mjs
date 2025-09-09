d8f7f67b6bdce06adb6e35f8a1dd39027d43c172
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-09-09 16:28:10 +0530

    Add login

--- users/controller.mjs
+++ users/controller.mjs
@@ -1,8 +1,9 @@
 import { ServerError } from '../error.mjs';
 import bcrypt from 'bcrypt';
 import prisma from '../prisma/db.mjs';
-import { errorPritify, UserSignupModel } from './validator.mjs';
+import { errorPritify, UserSignupModel, UserLoginModel } from './validator.mjs';
 import emailQueue from '../queue/email.queue.mjs';
+import { asyncJwtSign } from '../async.jwt.mjs';
 
 const signup = async (req, res, next) => {
   const result = await UserSignupModel.safeParseAsync(req.body);
@@ -19,9 +20,14 @@ const signup = async (req, res, next) => {
       password: hasedPassword,
     },
   });
-  console.log(newUser);
 
-  // TODO: create verification link
+  // 1. Add 2 columns in User table in DB.
+  // 1.1 Add resetToken(string), resetTokenExpiry(timestampz) in User prisma model
+  // 1.2 Run migration to acctually add column
+  // 2. generate a 32 keyword random string
+  // 3. update this string in DB with future 15min expiry time
+  // 4. make link example https://localhost:5000/resetPassword/fgvjkdsuhvgyahfvajdsfahvdsjvbd
+  // 5. add this above link email replacing http://google.com
 
   await emailQueue.add('welcome_email', {
     to: newUser.email,
@@ -35,19 +41,50 @@ const signup = async (req, res, next) => {
   res.json({ msg: 'signup is successful' });
 };
 
-const login = (req, res, next) => {
-  // TODO: find user by email from DB
+const login = async (req, res, next) => {
+  const result = await UserLoginModel.safeParseAsync(req.body);
+  if (!result.success) {
+    throw new ServerError(400, errorPritify(result));
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email,
+    },
+  });
+
   // TODO: check is account verified
-  // TODO: match hased password
-  // TODO: Generate JWT Token -> json web token
-  res.json({ msg: 'login done' });
+
+  if (!(await bcrypt.compare(req.body.password, user.password))) {
+    throw new ServerError(401, 'password mismatch');
+  }
+
+  const token = await asyncJwtSign(
+    { id: user.id, name: user.name, email: user.email },
+    process.env.TOKEN_SECRET
+  );
+
+  res.json({ msg: 'login successful', token });
 };
 
 const forgotPassword = (req, res, next) => {
+  // 1. find User via email from req.body
+  // 1. generate a 32 keyword random string
+  // 3. update this string in DB with future 15min expiry time
+  // 4. make link example https://localhost:5000/resetPassword/fgvjkdsuhvgyahfvajdsfahvdsjvbd
+  // 5. send this link via email
   res.json({ msg: 'forgot password' });
 };
+
 const resetPassword = (req, res, next) => {
-  res.json({ msg: 'reset password' });
+  // 1. Extract token from req.body
+  // 2. find User via token from DB
+  // 3. check for token expiry
+  // 4. check if is accountVerified
+  // 5. if account verified extract password from req.body
+  // 6. hash password
+  // 7. update user password in DB
+  res.json({ msg: 'reset password successul' });
 };
 
 export { signup, login, forgotPassword, resetPassword };
