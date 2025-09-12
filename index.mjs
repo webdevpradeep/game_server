d6475b1a039567179418c8beb5a4d7142b7e436a
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-09-12 15:00:06 +0530

    dummy route added

--- index.mjs
+++ index.mjs
@@ -4,11 +4,15 @@ dotenv.config();
 import express from 'express';
 import { errorHandler } from './error.mjs';
 import userRouter from './users/router.mjs';
+import gameRouter from './game/router.mjs';
+import sessionRouter from './session/router.mjs';
 const app = express();
 
 app.use(express.json());
 
 app.use('/users', userRouter);
+app.use('/games', gameRouter);
+app.use('/sessions', sessionRouter);
 
 app.all(/^.*$/, (req, res) => {
   res.status(400).json({ msg: "route dosen't exists" });
