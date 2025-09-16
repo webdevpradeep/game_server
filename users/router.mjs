3d5fabf554223ba32969730820dc8f5420c34a43
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-09-16 16:34:53 +0530

    multer config added

--- users/router.mjs
+++ users/router.mjs
@@ -6,14 +6,22 @@ import {
   login,
   resetPassword,
   signup,
+  updateProfileImage,
 } from './controller.mjs';
 import { authentication } from '../auth.mjs';
+import { singleImageUploadMiddleware } from '../storage/config.mjs';
 
 userRouter
   .post('/signup', signup)
   .post('/login', login)
   .patch('/forgotPassword', forgotPassword)
   .patch('/resetPassword', resetPassword)
-  .get('/profile', authentication, getMe);
+  .get('/profile', authentication, getMe)
+  .patch(
+    '/profile/image',
+    authentication,
+    singleImageUploadMiddleware('image'),
+    updateProfileImage
+  );
 
 export default userRouter;
