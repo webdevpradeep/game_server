941ad5e2e954cbfb44da027c94b79389b3cba838
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-09-25 16:32:47 +0530

    requestGame partially implemented

--- game/router.mjs
+++ game/router.mjs
@@ -1,10 +1,10 @@
 import express from 'express';
 const gameRouter = express.Router();
-import { addGame, listGame } from './controller.mjs';
+import { addGame, listGame, requestGame } from './controller.mjs';
 import { authentication } from '../auth.mjs';
 
 gameRouter.use(authentication);
 
-gameRouter.post('/', addGame).get('/', listGame);
+gameRouter.post('/', addGame).get('/', listGame).post('/request', requestGame);
 
 export default gameRouter;
