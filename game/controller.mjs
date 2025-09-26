3094f683c3690280367771b980eff25cafc958a4
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-09-26 16:58:57 +0530

    snake game and start code added

--- game/controller.mjs
+++ game/controller.mjs
@@ -1,5 +1,6 @@
 import { DB_ERR_CODES, prisma, Prisma } from '../prisma/db.mjs';
 import { ServerError } from '../error.mjs';
+import { spawn } from 'child_process';
 
 const addGame = async (req, res, next) => {
   // TODO: add validation
@@ -36,19 +37,13 @@ const requestGame = async (req, res, next) => {
       },
     });
   }
+  let gameSessionPlayer;
   try {
-    const gameSessionPlayer = await prisma.gameSessionPlayer.create({
+    gameSessionPlayer = await prisma.gameSessionPlayer.create({
       data: {
         sessionID: gameSession.id,
         playerID: req.user.id,
       },
-    });
-
-    res.json({
-      msg: 'successful',
-      gameID: req.body.gameID,
-      gameSession,
-      gameSessionPlayer,
     });
   } catch (err) {
     if (err.code === DB_ERR_CODES.UNIQUE_ERR) {
@@ -59,6 +54,53 @@ const requestGame = async (req, res, next) => {
     }
     throw err;
   }
+
+  const game = await prisma.game.findUnique({
+    where: {
+      id: req.body.gameID,
+    },
+  });
+  // find total number of players in this game session
+  const data = await prisma.gameSessionPlayer.aggregate({
+    where: {
+      sessionID: gameSession.id,
+    },
+    _count: {
+      playerID: true,
+    },
+  });
+
+  if (game.maxPlayer > data._count.playerID) {
+    return res.json({
+      msg: 'successful, Wait for other players to join',
+      gameID: req.body.gameID,
+      gameSession,
+      gameSessionPlayer,
+      data,
+    });
+  }
+
+  // start game
+  const gameInstance = spawn(
+    'node',
+    [
+      'D:/class/Backend/Game Server/game_server/allGames/snake/server/index.mjs',
+    ],
+    {
+      detached: true,
+      stdio: 'ignore',
+    }
+  );
+  gameInstance.unref();
+  console.log(gameInstance);
+
+  res.json({
+    msg: 'successful',
+    gameID: req.body.gameID,
+    gameSession,
+    gameSessionPlayer,
+    data,
+  });
 };
 
 export { addGame, listGame, requestGame };
