64972dc62eaa2bd917ee5ba5ed6369bac7fc67b4
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-09-30 17:32:41 +0530

    find game session info

--- game/router.mjs
+++ game/router.mjs
@@ -1,10 +1,19 @@
 import express from 'express';
 const gameRouter = express.Router();
-import { addGame, listGame, requestGame } from './controller.mjs';
+import {
+  addGame,
+  listGame,
+  requestGame,
+  getMyGameSession,
+} from './controller.mjs';
 import { authentication } from '../auth.mjs';
 
 gameRouter.use(authentication);
 
-gameRouter.post('/', addGame).get('/', listGame).post('/request', requestGame);
+gameRouter
+  .post('/', addGame)
+  .get('/', listGame)
+  .post('/request', requestGame)
+  .get('/session/:sessionID', getMyGameSession);
 
 export default gameRouter;
