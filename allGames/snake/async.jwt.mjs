37ad973ebce4fac20059371887a47f164589455c
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-10-11 15:18:17 +0530

    added incomplete sanke game token sharing

---
+++ allGames/snake/async.jwt.mjs
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+const asyncJwtSign = (payload, secret, options) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, secret, options, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(token);
+    });
+  });
+};
+
+const asyncJwtVerify = (token, secret) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (err, payload) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(payload);
+    });
+  });
+};
+
+export { asyncJwtSign, asyncJwtVerify };
