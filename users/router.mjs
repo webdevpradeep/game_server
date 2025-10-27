127229f03ad4e49a7b89969b59acda667b7803b2
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-10-27 19:47:09 +0530

    Add Google login route to user router

--- users/router.mjs
+++ users/router.mjs
@@ -1,19 +1,22 @@
 import express from 'express';
+import { authentication } from '../auth.mjs';
+import { singleImageUploadMiddleware } from '../storege/config.mjs';
 const userRouter = express.Router();
 import {
+  deleteProfileImage,
   forgotPassword,
   getMe,
+  googleLogin,
   login,
   resetPassword,
   signup,
   updateProfileImage,
 } from './controller.mjs';
-import { authentication } from '../auth.mjs';
-import { singleImageUploadMiddleware } from '../storage/config.mjs';
 
 userRouter
   .post('/signup', signup)
   .post('/login', login)
+  .post('/login/google', googleLogin)
   .patch('/forgotPassword', forgotPassword)
   .patch('/resetPassword', resetPassword)
   .get('/profile', authentication, getMe)
@@ -22,6 +25,7 @@ userRouter
     authentication,
     singleImageUploadMiddleware('image'),
     updateProfileImage
-  );
+  )
+  .delete('/profile/image/delete', authentication, deleteProfileImage);
 
 export default userRouter;
